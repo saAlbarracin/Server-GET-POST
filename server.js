1ab24292a23ac7ade7d9a065396b8434862fab05
Santiago <albarracinsantiago24@gmail.com>
2021-05-13 00:12:11 -0300

    Server Listo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const PUERTO = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/login", function (req, res) {
+  res.send(`Hola ${req.body.user} ${req.body.nickname} ${req.body.surname}`);
+});
+
+app.listen(3000, function () {
+  console.log("Servidor listo, iniciando en puerto 3000...");
+});
